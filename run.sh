b30913d91d3a883edb58558a811dff9f598667d9
LaurensGamet <73340705+LaurensGamet@users.noreply.github.com>
2024-08-20 16:59:10 +0200

    165920082024

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 current_datetime=$(date +"%H%M%d%m%Y")
 
+# Get in right directory
+cd /home/laurens/SomtodayAgenda
+
 # Get up to date
 git pull -q
 
 # Run main file
-sudo python3 main.py
+sudo python3 /home/laurens/SomtodayAgenda/main.py
 
 # Upload to Github
 git add -A
