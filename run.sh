7929e33a7971abaf5511d94138f08aac4a6a4136
Laurens Gamet <laurensgamet@gmail.com>
2024-08-20 15:41:40 +0200

    154120082024

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 current_datetime=$(date +"%H%M%d%m%Y")
 
+# Get up to date
+git pull 
+
+# Run main file
 sudo python3 main.py
 
+# Upload to Github
 git add -A
 git commit -q -m "$current_datetime"
 git push -q
