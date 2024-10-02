e318afb86f2aa75a5a7c46d56a3a97ed1362269f
LaurensGamet <73340705+LaurensGamet@users.noreply.github.com>
2024-10-02 22:00:04 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 current_datetime=$(date +"%H%M%d%m%Y")
 
-echo $(date + "%H"":""%M"" ""%d""-""%m""-""%Y")
+echo $(date +"%H"":""%M"" ""%d""-""%m""-""%Y")
 echo
 echo "# Get in right directory"
 cd /home/laurens/Somtoday_Laurens
@@ -23,6 +23,6 @@ sudo git add -A
 sudo git commit -m "$current_datetime"
 sudo git push
 
-echo
-echo
+echo  
+echo  
 
